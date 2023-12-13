75deb6a4ac1e9d79660f69f65cb62305f0134c9b
wenned <wenned@gmail.com>
2023-12-13 11:09:20 -0300

    criacao gradiente background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import NavBar from "./ui/dashboard/navbar";
 export default function Home() {
   return (
     <>
-    <main className="bg-gradient-to-r from-ws-l from-10% via-ws-c via-30% to-ws-r to-90%  grid grid-cols-1 grid-rows-4">
+    <main className="bg-gradient-to-r  from-ws-l  via-ws-c to-ws-r grid grid-cols-1 grid-rows-4">
       <nav className="h-screenbg-gradient-to-b from-ws flex flex-col justify-center">
         <Logo/>
         <NavBar/>
@@ -15,7 +15,7 @@ export default function Home() {
       <div id="Sobre" className="p-56 h-screen flex text-5xl justify-center items-center"><Sobre/></div>
       <div id="Projetos" className="h-screen flex text-5xl justify-center items-center">Projetos</div>
     </main>
-    <footer className=" bg-gradient-to-r from-ws-l from-10% via-ws-c via-30% to-ws-r to-90%  h-96 flex text-5xl justify-center items-center">
+    <footer className=" bg-gradient-to-r from-ws-l via-ws-c to-ws-r to-90%  h-96 flex text-5xl justify-center items-center">
       <Contato/>
     </footer>
     </>
