ec8d3e8b79f723cf37e8e3dd29f53ee9bf8db240
wenned <wenned@gmail.com>
2023-12-21 17:16:49 -0300

    atualizacao correcao de grafia

--- src/app/ui/dashboard/Sobre.tsx
+++ src/app/ui/dashboard/Sobre.tsx
@@ -16,15 +16,15 @@ export default function Sobre (){
 
             <div className="text-justify p-6 max-sm:p-2 max-sm:space-y-0 space-y-3">
                 <p>Sou uma pessoa dinâmica, aberto a novas experiências, sempre tentando ser pontual e cumprir compromissos, nascido em,
-                <strong>Cândido Mendes, MA</strong>, mudei-me para <strong>Imperatriz, MA</strong> aos 16 anos, sempre com objetivo de me especializar na area da tecnologia,
-                 mesmo naquela época não sabendo para qual área realmente focar, sempre me adaptei facilmente em varias áreas da programação tanto, <strong>Programação Back-end</strong>,
+                <strong> Cândido Mendes, MA</strong>, mudei-me para <strong>Imperatriz, MA</strong> aos 16 anos, sempre com objetivo de engressar na area da tecnologia,
+                 onde hoje aos 34 anos posso finalmente dar andamento ao meu objetivo de carreira, sempre me adaptei facilmente em varias áreas da programação tanto, <strong>Programação Back-end</strong>,
                  onde foi meu primeiro contato com programação, e agora nessa nova fase com <strong>Programação Fronte-end</strong></p>
                 
-                <p><strong>Hobbies:</strong> Estudar programação, <strong>"JavaScript", "Python", "JAVA", "POSTGRESQL", "MongoDB"</strong>. Desemvolvimento Web: <strong>"NODE.JS", "REACT", "NEXT.JS", "HTML", "CSS"</strong>.</p>
+                <p><strong>Hobbies:</strong> Estudar programação, <strong>"Python", "JAVA", "JavaScript", "POSTGRESQL", "MongoDB"</strong>. Desemvolvimento Web: <strong>"NODE.JS", "REACT", "NEXT.JS", "HTML", "CSS"</strong>.</p>
             </div> 
 
             <div className="text-justify">
-                <p>"Tenho trabalhado em projetos pessoais para desenvolver minhas habilidades em programação. Entre elas <strong>React, JavaScript, Python e Postgresql.</strong> Durante o processo, estou aprendendo a lidar com autenticação de usuários e armazenamento de dados. Além disso, fui capaz de solucionar problemas técnicos por conta própria, destacando minha capacidade de pensar de forma crítica e encontrar soluções. Estou animado para continuar aprendendo e aplicando meus conhecimentos em futuros projetos."</p>
+                <p>"Tenho trabalhado em projetos pessoais para desenvolver minhas habilidades em programação. Entre elas <strong>React, NextJS JavaScript, Python, Java e Postgresql.</strong> Durante o processo, estou aprendendo a lidar com autenticação de usuários e armazenamento de dados. Além disso, fui capaz de solucionar problemas técnicos por conta própria, destacando minha capacidade de pensar de forma crítica e encontrar soluções. Estou animado para continuar aprendendo e aplicando meus conhecimentos em futuros projetos."</p>
             </div>
 
         </section>
