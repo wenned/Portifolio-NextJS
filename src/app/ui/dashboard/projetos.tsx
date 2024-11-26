c056bc8cd778f1403af3b1b965e7b66203dad0d7
exprox <wenned@gmail.com>
2024-11-26 10:40:20 -0300

    criacao botao de acao

--- src/app/ui/dashboard/projetos.tsx
+++ src/app/ui/dashboard/projetos.tsx
@@ -60,18 +60,17 @@ export default function Projetos(){
             <div className="flex flex-col gap-9">{stateButton == 2? <RestAPI/>: ''}</div>
 
             {/* botao lateral */}
-            <div  className={stateButton==0?"hidden": "fixed bottom-[50%] right-0 z-0 border-l-2 border-ws-new w-44 h-28 flex justify-center items-center text-3xl flex-col gap-5"}>
-                <span onClick={()=>{ActiveProjeto('Front-End')}} className="justify-center cursor-pointer border-r-2 border-ws-new w-36 animate-retorno overflow-hidden whitespace-nowrap flex hover:bg-gradient-to-r from-transparent via-ws-projeto to-transparent hover:text-black">Front-End</span>
-                <span onClick={()=>{ActiveProjeto('Back-End')}}  className="justify-center cursor-pointer border-r-2 border-ws-new w-36 animate-retorno overflow-hidden whitespace-nowrap flex hover:bg-gradient-to-r from-transparent via-ws-projeto to-transparent hover:text-black">Back-End</span>
+            <div  className={stateButton==0?"hidden": "max-[820px]:bg-white max-[820px]:rounded-full max-[820px]:w-14 max-[820px]:h-14  fixed bottom-[50%] right-0 z-0 border-l-2 border-ws-new w-44 h-28 flex justify-center items-center text-3xl flex-col gap-5"}>
+                <span onClick={()=>{ActiveProjeto('Front-End')}} className="justify-center cursor-pointer border-r-2 max-[820px]:border-none border-ws-new w-36 animate-retorno overflow-hidden whitespace-nowrap flex hover:bg-gradient-to-r from-transparent via-ws-projeto to-transparent hover:text-black">Front-End</span>
+                <span onClick={()=>{ActiveProjeto('Back-End')}}  className="justify-center cursor-pointer border-r-2 max-[820px]:border-none border-ws-new w-36 animate-retorno overflow-hidden whitespace-nowrap flex hover:bg-gradient-to-r from-transparent via-ws-projeto to-transparent hover:text-black">Back-End</span>
             </div>
             
             {/* botal principal de entrada */}
-            <div id="Projetos" className={stateButton == 0 ?'border-l-2 border-ws-new flex w-11/12 h-screen items-center flex-wrap pb-8 flex-col justify-center gap-y-5 pt-[1.5em] -tracking-wide': 'hidden'}>
+            <div id="Projetos" className={stateButton == 0 ?'border-l-2 border-ws-new flex w-11/12 h-screen items-center flex-wrap pb-8 flex-col justify-center gap-y-5 pt-[1.5em] -tracking-wide max-[820px]:text-[0.87em]':'hidden'}>
                 <span onClick={()=>{ActiveProjeto('Front-End')}} className="cursor-pointer w-2/3 h-[1em] justify-center items-center flex hover:bg-gradient-to-r from-transparent via-ws-projeto to-transparent hover:text-black max-sm:text-[0.37em] max-sm:w-11/12">Front-End</span>
                 <span className="bg-gradient-to-r from-transparent via-ws-new to-transparent w-2/3 h-[2px]"></span>
                 <span onClick={()=>{ActiveProjeto('Back-End')}}  className="cursor-pointer w-2/3 h-[1em] justify-center items-center flex hover:bg-gradient-to-r from-transparent via-ws-projeto to-transparent hover:text-black max-sm:text-[0.37em] max-sm:w-10/12">Back-End</span>
             </div>
         </section>
-
     )
 }
