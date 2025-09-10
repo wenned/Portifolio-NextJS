6d7640513db0a41faf4a66e50e1c384b1d113428
exprox <wenned@gmail.com>
2025-09-10 10:58:41 -0300

    atualizacao de espacamentos e alinhamentos

--- src/app/ui/dashboard/Sobre.tsx
+++ src/app/ui/dashboard/Sobre.tsx
@@ -1,8 +1,8 @@
 export default function Sobre (){
 
     return(
-        <section itemID="Sobre" className="text-white max-sm:h-auto border-r-2 relative max-sm:border-none border-ws-new pr-3 m-6 max-sm:m-2 text-2xl max-xl:text-xl max-[478px]:text-sm max-[848px]:text-base flex flex-col " id="Sobre">
-            <div className="flex justify-end w-full items-end -tracking-tighter text-7xl">Sobre</div>
+        <section itemID="Sobre" className="text-white max-sm:h-auto border-r-2 relative max-sm:border-none border-ws-new max-sm:m-2 text-2xl max-xl:text-xl max-[478px]:text-sm max-[848px]:text-base flex flex-col " id="Sobre">
+            <div className="flex justify-end w-full items-end -tracking-[-0.1em] text-7xl">Sobre</div>
             <div className="border-r-2 border-ws-new max-sm:border-none ">
                 <p><strong>Nome: </strong>Wenned Silva</p>
                 <p><strong>Formação</strong></p>
@@ -13,7 +13,7 @@ export default function Sobre (){
                 <p><strong>Idioma :</strong> Portugues, Ingles - <small>Cursando</small></p>
             </div>
 
-            <div className="text-justify p-6 max-sm:p-2 max-sm:space-y-0 space-y-3">
+            <div className="text-justify p-6 max-sm:pl-2 max-sm:space-y-0 space-y-3">
                 <p>Sou uma pessoa dinâmica, aberto a novas experiências, nascido em,<strong> Cândido Mendes, MA</strong>, mudei-me para <strong>Imperatriz, MA</strong> aos 16 anos, sempre com objetivo de engressar na area da tecnologia,
                  onde hoje aos 34 anos posso finalmente dar andamento ao meu objetivo de carreira, sempre me adaptei facilmente em varias áreas da programação, na<strong>Programação Back-end</strong>,
                  onde foi meu primeiro contato, e agora nessa nova fase com <strong>Programação Fronte-end.</strong></p>
@@ -21,7 +21,7 @@ export default function Sobre (){
                 <p><strong>Hobbies:</strong> Estudar programação, <strong>"Python", "JAVA", "JavaScript", "POSTGRESQL", "MongoDB"</strong>. Desemvolvimento Web: <strong>"NODE.JS", "REACT", "NEXT.JS", "HTML", "CSS"</strong>.</p>
             </div> 
 
-            <div className="text-justify">
+            <div className="text-justify max-sm:p-2">
                 <p>"Tenho trabalhado em projetos pessoais para desenvolver minhas habilidades em programação. Entre elas <strong>React, NextJS JavaScript, Python, Java e Postgresql.</strong> Durante o processo, estou aprendendo a lidar com autenticação de usuários e armazenamento de dados. Além disso, fui capaz de solucionar problemas técnicos por conta própria, destacando minha capacidade de pensar de forma crítica e encontrar soluções. Estou animado para continuar aprendendo e aplicando meus conhecimentos em futuros projetos."</p>
             </div>
         </section>
